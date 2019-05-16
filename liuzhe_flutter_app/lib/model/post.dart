7b4361697434679d85be70ec6968d662295c94c0
liu <liuzhe@k12cloud.cn>
2019-05-16 16:44:04 +0800

    ListView内容展示

---
+++ liuzhe_flutter_app/lib/model/post.dart
@@ -0,0 +1,28 @@
+class Post{
+  const Post({
+    this .title,
+    this.author,
+    this.imageUrl
+});
+  final String title;
+  final String author;
+  final String imageUrl;
+}
+
+final List<Post> posts = [
+  Post(
+    title: "大侦探皮卡丘",
+    author: 'Mohamed Chahin',
+    imageUrl: 'http://img3.doubanio.com/view/photo/s_ratio_poster/public/p2555538168.jpg',
+  ),
+  Post(
+    title: "复仇者联盟4：终局之战",
+    author: 'Mohamed Chahin',
+    imageUrl: 'http://img3.doubanio.com/view/photo/s_ratio_poster/public/p2552058346.jpg',
+  ),
+  Post(
+    title: '何以为家',
+    author: 'Mohamed Chahin',
+    imageUrl: 'http://img3.doubanio.com/view/photo/s_ratio_poster/public/p2555295759.jpg' ,
+  ),
+];
